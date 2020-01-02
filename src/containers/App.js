e102e06b2940f3dace2f361d6d1680d0080ca994
Francis Njuguna <coulsorfrancois@gmail.com>
2020-01-02 16:17:27 +0300

    Wrap the app with the BrowserRouter.

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 import './App.css';
 import Navbar from '../components/Navbar'
+import {BrowserRouter} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 
 function App(props) {
     return(
+      <BrowserRouter>
         <div>
             <Navbar/>
         </div>
+      </BrowserRouter>
     )
 }
 
