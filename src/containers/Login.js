30a907960c3b703974f0e2e9b7e6e1dbed223576
Francis Njuguna <coulsorfrancois@gmail.com>
2020-01-03 18:59:11 +0300

    Add the name attribute to the Login and Signup inputs.

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -9,7 +9,7 @@ const Login = (props)=>{
 
                     <div className="row">
                         <div className="input-field col s12">
-                        <input id="email" type="email" className="validate" data-testid="email" required={true}/>
+                        <input id="email" name="email" type="email" className="validate" data-testid="email" required={true}/>
                         <label htmlFor="email">Email</label>
                         <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
                         </div>
@@ -17,7 +17,7 @@ const Login = (props)=>{
 
                     <div className="row">
                         <div className="input-field col s12">
-                            <input id="password" type="password" className="validate" data-testid="password" required={true}/>
+                            <input id="password" name="password" type="password" className="validate" data-testid="password" required={true}/>
                             <label htmlFor="password">Password</label>
                             <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
                         </div>
--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -10,12 +10,12 @@ const Signup = (props)=>{
 
                     <div className="row">
                         <div className="input-field col s6">
-                            <input id="firstName" type="text" className="validate" data-testid="firstName" required={true}/>
+                            <input id="firstName" name="firstName" type="text" className="validate" data-testid="firstName" required={true}/>
                             <label htmlFor="firstName">First Name</label>
                             <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
                         </div>
                         <div className="input-field col s6">
-                            <input id="lastName" type="text" className="validate" data-testid="lastName" required={true} />
+                            <input id="lastName" name="lastName" type="text" className="validate" data-testid="lastName" required={true} />
                             <label htmlFor="lastName">Last Name</label>
                             <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
                         </div>
@@ -24,13 +24,13 @@ const Signup = (props)=>{
                     <div className="row">
                         
                         <div className="input-field col s6">
-                            <input  id="homeAddress" type="text" className="validate" data-testid="homeAddress" required={true}/>
+                            <input  id="homeAddress" name="homeAddress" type="text" className="validate" data-testid="homeAddress" required={true}/>
                             <label htmlFor="homeAddress">Home Address</label>
                             <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
                         </div>
 
                         <div className="input-field col s6">
-                            <input id="workAddress" type="text" className="validate" data-testid="workAddress" required={true} />
+                            <input id="workAddress" name="workAddress" type="text" className="validate" data-testid="workAddress" required={true} />
                             <label htmlFor="workAddress">Last Name</label>
                             <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
                         </div>
@@ -38,7 +38,7 @@ const Signup = (props)=>{
 
                     <div className="row">
                         <div className="input-field col s12">
-                            <input id="email" type="email" className="validate" data-testid="email" required={true} />
+                            <input id="email" type="email" name="email" className="validate" data-testid="email" required={true} />
                             <label htmlFor="email">Email</label>
                             <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
                         </div>
@@ -46,12 +46,12 @@ const Signup = (props)=>{
                     
                     <div className="row">
                         <div className="input-field col s6">
-                            <input id="password" type="password" className="validate" data-testid="password" required={true}/>
+                            <input id="password" name="password" type="password" className="validate" data-testid="password" required={true}/>
                             <label htmlFor="password">Password</label>
                             <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
                         </div>
                         <div className="input-field col s6">
-                            <input id="confirmPassword" type="password" className="validate" required={true} data-testid="confirmPassword" />
+                            <input id="confirmPassword" name="confirmPassword" type="password" className="validate" required={true} data-testid="confirmPassword" />
                             <label htmlFor="confirmPassword">Confirm Password</label>
                             <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
                         </div>
