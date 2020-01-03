68507bc616ccfc6e3c9da28845de674e50822954
Francis Njuguna <coulsorfrancois@gmail.com>
2020-01-03 18:36:52 +0300

    Successfully test the login and signup route.

--- src/components/SignedOutLinks.js
+++ src/components/SignedOutLinks.js
@@ -5,8 +5,12 @@ const SignedOutLinks = (props)=>{
     return(
 
         <ul id="nav-mobile" className="right hide-on-med-and-down" data-testid="signedOutUl">
-                <li><NavLink to="/login">Log In</NavLink></li>
-                <li><NavLink to="/signup">Sign Up</NavLink></li>
+                <li>
+                    <NavLink to="/login" data-testid="loginLink">Log In</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/signup" data-testid="signupLink">Sign Up</NavLink>
+                </li>
         </ul>
     )
 } 
