484b08a41ca69aa3308ef6fa2840776dab503465
Francis Njuguna <coulsorfrancois@gmail.com>
2020-01-03 17:48:59 +0300

    Successfully writes and runs a test to confirm that the signup component has the firstName,lastName, homeAddress, workAddress, email, password and confirmPassword input and they have required=true attribute

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -10,45 +10,50 @@ const Signup = (props)=>{
 
                     <div className="row">
                         <div className="input-field col s6">
-                            <input id="first_name" type="text" className="validate"/>
-                            <label htmlFor="first_name">First Name</label>
+                            <input id="firstName" type="text" className="validate" data-testid="firstName" required={true}/>
+                            <label htmlFor="firstName">First Name</label>
+                            <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
                         </div>
                         <div className="input-field col s6">
-                            <input id="last_name" type="text" className="validate"/>
-                            <label htmlFor="last_name">Last Name</label>
+                            <input id="lastName" type="text" className="validate" data-testid="lastName" required={true} />
+                            <label htmlFor="lastName">Last Name</label>
+                            <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
                         </div>
                     </div>
 
                     <div className="row">
                         
                         <div className="input-field col s6">
-                            <input  id="homeAddress" type="text" className="validate"/>
+                            <input  id="homeAddress" type="text" className="validate" data-testid="homeAddress" required={true}/>
                             <label htmlFor="homeAddress">Home Address</label>
+                            <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
                         </div>
 
                         <div className="input-field col s6">
-                            <input id="workAddress" type="text" className="validate"/>
+                            <input id="workAddress" type="text" className="validate" data-testid="workAddress" required={true} />
                             <label htmlFor="workAddress">Last Name</label>
+                            <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
                         </div>
                     </div>
 
                     <div className="row">
                         <div className="input-field col s12">
-                            <input id="email" type="email" className="validate"/>
+                            <input id="email" type="email" className="validate" data-testid="email" required={true} />
                             <label htmlFor="email">Email</label>
+                            <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
                         </div>
                     </div>
-
+                    
                     <div className="row">
-                        <div className="input-field col s12">
-                            <input id="password" type="password" className="validate"/>
+                        <div className="input-field col s6">
+                            <input id="password" type="password" className="validate" data-testid="password" required={true}/>
                             <label htmlFor="password">Password</label>
+                            <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
                         </div>
-                    </div>
-                    <div className="row">
-                        <div className="input-field col s12">
-                            <input id="password" type="password" className="validate"/>
-                            <label htmlFor="password">Password</label>
+                        <div className="input-field col s6">
+                            <input id="confirmPassword" type="password" className="validate" required={true} data-testid="confirmPassword" />
+                            <label htmlFor="confirmPassword">Confirm Password</label>
+                            <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
                         </div>
                     </div>
 
--- src/containers/Signup.test.js
+++ src/containers/Signup.test.js
@@ -5,3 +5,36 @@ import Signup from './Signup';
 test('test the rendering of the Signup component', () => {
     render(<Signup />);
 });
+
+test('test that the signup component has the firstName,lastName, homeAddress, workAddress, email, password and confirmPassword input and they have required=true attribute', 
+    () => {
+            let {getByTestId}=render(<Signup />);
+            let firstName = getByTestId('firstName');
+            let lastName = getByTestId('lastName');
+            let homeAddress = getByTestId('homeAddress');
+            let workAddress = getByTestId('workAddress');
+            let email = getByTestId('email');
+            let password = getByTestId('password');
+            let confirmPassword = getByTestId('confirmPassword');
+
+            //expect the inputs are there/
+            expect(firstName).toBeInTheDocument();
+            expect(lastName).toBeInTheDocument();
+            expect(homeAddress).toBeInTheDocument();
+            expect(workAddress).toBeInTheDocument();
+            expect(email).toBeInTheDocument();
+            expect(firstName).toBeInTheDocument();
+            expect(password).toBeInTheDocument();
+            expect(confirmPassword).toBeInTheDocument();
+            //expect the inputs to be required
+            expect(firstName).toBeRequired();
+            expect(lastName).toBeRequired();
+            expect(homeAddress).toBeRequired();
+            expect(workAddress).toBeRequired();
+            expect(email).toBeRequired();
+            expect(firstName).toBeRequired();
+            expect(password).toBeRequired();
+            expect(confirmPassword).toBeRequired();
+            
+        }
+);
