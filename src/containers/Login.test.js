60dfdba66d4be57b0e7ffd4d129241b3aac0375e
Francis Njuguna <coulsorfrancois@gmail.com>
2020-01-03 19:15:15 +0300

    Test that the name attribute for the respective inputs for Signup and Login Component is provided.

--- src/containers/Login.test.js
+++ src/containers/Login.test.js
@@ -6,12 +6,17 @@ test('successful rendering of the Login component', () => {
     render(<Login />);
 });
 
-test("Test if the email and password inputs are present", ()=>{
+test("Test if the email and password inputs are present, have the required=true attribute and have name attribute with correct name", ()=>{
     let {getByTestId} = render(<Login/>);
     let email = getByTestId('email');
-    let login = getByTestId('password');
+    let password = getByTestId('password');
+    //test if the email and password is in the document
     expect(email).toBeInTheDocument();
-    expect(login).toBeInTheDocument();
+    expect(password).toBeInTheDocument();
+    //test if the email and password input is required
     expect(email).toBeRequired();
-    expect(login).toBeRequired();
+    expect(password).toBeRequired();
+    //test if they have name attribute with there corresponding values
+    expect(email).toHaveAttribute('name', 'email');
+    expect(password).toHaveAttribute('name', 'password');
 });
--- src/containers/Signup.test.js
+++ src/containers/Signup.test.js
@@ -6,7 +6,7 @@ test('test the rendering of the Signup component', () => {
     render(<Signup />);
 });
 
-test('test that the signup component has the firstName,lastName, homeAddress, workAddress, email, password and confirmPassword input and they have required=true attribute', 
+test('test that the signup component has the firstName,lastName, homeAddress, workAddress, email, password and confirmPassword input and they have required=true attribute and name attribute.', 
     () => {
             let {getByTestId}=render(<Signup />);
             let firstName = getByTestId('firstName');
@@ -26,6 +26,7 @@ test('test that the signup component has the firstName,lastName, homeAddress, wo
             expect(firstName).toBeInTheDocument();
             expect(password).toBeInTheDocument();
             expect(confirmPassword).toBeInTheDocument();
+
             //expect the inputs to be required
             expect(firstName).toBeRequired();
             expect(lastName).toBeRequired();
@@ -35,6 +36,16 @@ test('test that the signup component has the firstName,lastName, homeAddress, wo
             expect(firstName).toBeRequired();
             expect(password).toBeRequired();
             expect(confirmPassword).toBeRequired();
+
+            //expect the inputs to have name attribute
+            expect(firstName).toHaveAttribute('name','firstName');
+            expect(lastName).toHaveAttribute('name','lastName');
+            expect(homeAddress).toHaveAttribute('name','homeAddress');
+            expect(workAddress).toHaveAttribute('name','workAddress');
+            expect(email).toHaveAttribute('name','email');
+            expect(firstName).toHaveAttribute('name','firstName');
+            expect(password).toHaveAttribute('name','password');
+            expect(confirmPassword).toHaveAttribute('name','confirmPassword');
             
         }
 );
