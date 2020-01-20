bfa89423e25082bb3ff37bfa5d3d075bb3fb0ea3
Francis Njuguna <coulsorfrancois@gmail.com>
2020-01-20 11:00:11 +0300

    Rename the REMOVE_ERROR constant action type to REMOVE_ERRORS.

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -2,4 +2,4 @@
 export const LOGIN = 'LOGIN';
 export const LOGOUT = 'LOGOUT';
 export const ADD_ERRORS = 'ADD_ERRORS';
-export const REMOVE_ERROR = 'REMOVE_ERROR';
+export const REMOVE_ERRORS = 'REMOVE_ERRORS';
