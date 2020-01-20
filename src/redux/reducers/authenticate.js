fc29da8eb7ebfe3482804c3ba0820e9cab6fb034
Francis Njuguna <coulsorfrancois@gmail.com>
2020-01-20 12:16:30 +0300

    Split the reducer to authenticate reducer that handles the signedIn state property with the LOGIN and LOGOUT actions.

---
+++ src/redux/reducers/authenticate.js
@@ -0,0 +1,18 @@
+import { LOGIN, LOGOUT } from 
+  '../actions';
+
+
+
+const authenticate = (state=false, action)=>{
+
+    switch(action.type){
+        case LOGIN:
+           return true
+        case LOGOUT:
+           return false
+        default:
+           return state
+    }
+};
+
+export default authenticate;
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,25 +1,31 @@
 import { LOGIN, LOGOUT,ADD_ERRORS, REMOVE_ERRORS } from 
   '../actions';
-
+import authenticate from './authenticate';
 
 const initialState = {
     signedIn: false,
     errors: []
 }
 
-const ujumbeApp = (state = initialState, action)=>{
-    switch(action.type){
-        case LOGIN:
-            return{ ...state, signedIn:true }
-        case LOGOUT:
-            return Object.assign({}, state, {signedIn: false})
-        case ADD_ERRORS:
-            return {...state, errors: action.errors}
-        case REMOVE_ERRORS:
-            return {...state, errors: []}
-        default:
-            return state
+const ujumbeApp = (state={}, action)=>{
+    return{
+        signedIn: authenticate(state.signedIn, action)
     }
-}
+};
 
-export default ujumbeApp
+// const ujumbeApp = (state = initialState, action)=>{
+//     switch(action.type){
+//         case LOGIN:
+//             return{ ...state, signedIn:true }
+//         case LOGOUT:
+//             return Object.assign({}, state, {signedIn: false})
+//         case ADD_ERRORS:
+//             return {...state, errors: action.errors}
+//         case REMOVE_ERRORS:
+//             return {...state, errors: []}
+//         default:
+//             return state
+//     }
+// }
+
+export default ujumbeApp;
