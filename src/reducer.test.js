8b636610220c7d1b88f741414de4dc39da16526e
Francis Njuguna <coulsorfrancois@gmail.com>
2020-01-20 13:27:56 +0300

    Test if the returned state after firing REMOVE_ERROR action to not contain any errors if the previous state had errors.

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,5 +1,6 @@
 import ujumbeAppRedux from './redux/reducers/index';
 import addErrors from './redux/actionCreators/addErrors';
+import removeErrors from './redux/actionCreators/removeErrors';
 
 describe('Reducer', () => {
     
@@ -11,7 +12,7 @@ describe('Reducer', () => {
                 expect(state).toEqual(expectedState);
         }); 
 
-        it('Expect the return state after firing a ADD_ERROR action to contain the errors', ()=>{
+        it('Expect the return state after firing a ADD_ERROR action to contain same errors passed in the addError action creator.', ()=>{
             const expectedErrors = [
                 {
                     errorName: 'ValidationError',
@@ -28,6 +29,28 @@ describe('Reducer', () => {
             ];
             let state= ujumbeAppRedux(undefined, addErrors(expectedErrors));
             expect(state.errors).toEqual(expectedErrors);
+        });
+
+        it('Expect the returned state after firing REMOVE_ERROR action to not contain any errors if the previous state had errors', ()=>{
+            const expectedErrors = [
+                {
+                    errorName: 'ValidationError',
+                    errorMessage: "FirstName is required"
+                },
+                {
+                    errorName: 'ValidationError',
+                    errorMessage: "LastName is required"
+                },
+                {
+                    errorName: 'ValidationError',
+                    errorMessage: "Age is required"
+                }
+            ];
+            let initialState = {
+                errors: expectedErrors
+            }
+            let state = ujumbeAppRedux(initialState, removeErrors());
+            expect(state.errors).toEqual([]);
         });
 
     });
