10e099d9992608ab6fe9e52361ff77f28a41c067
Francis Njuguna <coulsorfrancois@gmail.com>
2020-01-29 23:37:49 +0300

    Add the startCall action creator.

---
+++ src/redux/actionCreators/startCall.js
@@ -0,0 +1,9 @@
+import {START_CALL} from '../actionTypes';
+
+const startCall = ()=>{
+    return {
+        type: START_CALL
+    }
+};
+
+export default startCall;
--- src/redux/actionTypes.js
+++ src/redux/actionTypes.js
@@ -3,3 +3,4 @@ export const LOGIN = 'LOGIN';
 export const LOGOUT = 'LOGOUT';
 export const ADD_ERRORS = 'ADD_ERRORS';
 export const REMOVE_ERRORS = 'REMOVE_ERRORS';
+export const START_CALL  = 'START_CALL';
