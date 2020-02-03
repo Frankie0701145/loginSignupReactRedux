2819390e96eaae6c6c180e21857c96b97cf8cf09
Francis Njuguna <coulsorfrancois@gmail.com>
2020-02-03 22:28:15 +0300

    Remove the unused code

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,13 +1,9 @@
-import { LOGIN, LOGOUT,ADD_ERRORS, REMOVE_ERRORS } from 
-  '../actionTypes';
+
 import authenticate from './authenticate';
 import addRemoveErrors from './addRemoveErrors';
 import startEndCall from './startEndCall';
 
-// const initialState = {
-//     signedIn: false,
-//     errors: []
-// }
+
 
 const ujumbeApp = (state={}, action)=>{
     return{
@@ -17,19 +13,6 @@ const ujumbeApp = (state={}, action)=>{
     }
 };
 
-// const ujumbeApp = (state = initialState, action)=>{
-//     switch(action.type){
-//         case LOGIN:
-//             return{ ...state, signedIn:true }
-//         case LOGOUT:
-//             return Object.assign({}, state, {signedIn: false})
-//         case ADD_ERRORS:
-//             return {...state, errors: action.errors}
-//         case REMOVE_ERRORS:
-//             return {...state, errors: []}
-//         default:
-//             return state
-//     }
-// }
+
 
 export default ujumbeApp;
