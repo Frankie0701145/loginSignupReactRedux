448ff7fea31acdd74e0eaf490d2e56a8085c2b1f
Francis Njuguna <coulsorfrancois@gmail.com>
2020-02-03 22:17:13 +0300

    Added the confirmPassword input and tested if it is displayed

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,36 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-
-const Login = (props)=>{
-    return(
-        <div className="container"> 
-            <div className="row">
-                <form className="col s12">
-
-                    <div className="row">
-                        <div className="input-field col s12">
-                        <input id="email" name="email" type="email" className="validate" data-testid="email" required={true}/>
-                        <label htmlFor="email">Email</label>
-                        <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
-                        </div>
-                    </div>
-
-                    <div className="row">
-                        <div className="input-field col s12">
-                            <input id="password" name="password" type="password" className="validate" data-testid="password" required={true}/>
-                            <label htmlFor="password">Password</label>
+class Login extends Component {
+    render(){
+        return(
+            <div className="container"> 
+                <div className="row">
+                    <form className="col s12">
+    
+                        <div className="row">
+                            <div className="input-field col s12">
+                            <input id="email" name="email" type="email" className="validate" data-testid="email" required={true}/>
+                            <label htmlFor="email">Email</label>
                             <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
+                            </div>
                         </div>
-                    </div>
-                    <div className="row">
-                        <div className="input-field col s12" >
-                            <button className="waves-effect waves-light btn" data-testid="loginBtn">Login</button>
+    
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <input id="password" name="password" type="password" className="validate" data-testid="password" required={true}/>
+                                <label htmlFor="password">Password</label>
+                                <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
+                            </div>
                         </div>
-                    </div>
-                </form>
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <input id="confirmPassword" name="confirmPassword" type="password" className="validate" data-testid="confirmPassword" required={true}/>
+                                <label htmlFor="confirmPassword">Confirm Password</label>
+                                <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="input-field col s12" >
+                                <button className="waves-effect waves-light btn" data-testid="loginBtn">Login</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
+
 
 export default Login;
--- src/test/Login.test.js
+++ src/test/Login.test.js
@@ -10,13 +10,17 @@ test("Test if the email and password inputs are present, have the required=true 
     let {getByTestId} = render(<Login/>);
     let email = getByTestId('email');
     let password = getByTestId('password');
+    let confirmPassword = getByTestId('confirmPassword');
     //test if the email and password is in the document
     expect(email).toBeInTheDocument();
     expect(password).toBeInTheDocument();
+    expect(confirmPassword).toBeInTheDocument();
     //test if the email and password input is required
     expect(email).toBeRequired();
     expect(password).toBeRequired();
+    expect(confirmPassword).toBeRequired();
     //test if they have name attribute with there corresponding values
     expect(email).toHaveAttribute('name', 'email');
     expect(password).toHaveAttribute('name', 'password');
+    expect(confirmPassword).toHaveAttribute('name','confirmPassword');
 });
