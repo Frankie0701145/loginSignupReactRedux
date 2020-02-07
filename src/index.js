e61600f187bf609fd84feb75dd905a3dddf19dcd
Francis Njuguna <coulsorfrancois@gmail.com>
2020-02-07 12:35:00 +0300

    Export the store.

--- src/index.js
+++ src/index.js
@@ -9,12 +9,12 @@ import {Provider} from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
 
 
-const store = createStore(
+export const store = createStore(
     ujumbeApp,
     applyMiddleware(thunkMiddleware)
-  );
+);
 
-ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}> <App/> </Provider> , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
