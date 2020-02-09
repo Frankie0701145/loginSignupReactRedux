a4343601f3cd081168271ddd516d1c6cc19a7ef4
Francis Njuguna <coulsorfrancois@gmail.com>
2020-02-09 19:02:16 +0300

    Connect the addErrors and removeErrors actions to the Signup component and turn the Signup component to state component.

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -1,73 +1,89 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { connect } from 'react-redux';
+import addErrors from '../redux/actionCreators/addErrors';
+import removeErrors from '../redux/actionCreators/removeErrors';
 
+export class Signup extends Component{
 
-export const Signup = (props)=>{
+    submit = (e)=>{
+        e.preventDefault();
+        let userDetails = {
+            firstName: e.target.firstName.value,
+            lastName: e.target.lastName.value,
+            email: e.target.email.value,
+            homeAddress: e.target.homeAddress.value,
+            workAddress: e.target.workAddress.value,
+            password: e.target.password.value
+        }
+        console.log(userDetails);
+    }
 
-    return(
-        <div className="container"> 
-              <div className="row">
-                <form className="col s12" method="POST">
+    render(){
+        return(
+            <div className="container"> 
+                <div className="row">
+                    <form className="col s12" method="POST" onSubmit={this.submit}>
 
-                    <div className="row">
-                        <div className="input-field col s6">
-                            <input id="firstName" name="firstName" type="text" className="validate" data-testid="firstName" required={true}/>
-                            <label htmlFor="firstName">First Name</label>
-                            <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
+                        <div className="row">
+                            <div className="input-field col s6">
+                                <input id="firstName" name="firstName" type="text" className="validate" data-testid="firstName" required={true}/>
+                                <label htmlFor="firstName">First Name</label>
+                                <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
+                            </div>
+                            <div className="input-field col s6">
+                                <input id="lastName" name="lastName" type="text" className="validate" data-testid="lastName" required={true} />
+                                <label htmlFor="lastName">Last Name</label>
+                                <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
+                            </div>
                         </div>
-                        <div className="input-field col s6">
-                            <input id="lastName" name="lastName" type="text" className="validate" data-testid="lastName" required={true} />
-                            <label htmlFor="lastName">Last Name</label>
-                            <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
-                        </div>
-                    </div>
 
-                    <div className="row">
+                        <div className="row">
+                            
+                            <div className="input-field col s6">
+                                <input  id="homeAddress" name="homeAddress" type="text" className="validate" data-testid="homeAddress" required={true}/>
+                                <label htmlFor="homeAddress">Home Address</label>
+                                <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
+                            </div>
+
+                            <div className="input-field col s6">
+                                <input id="workAddress" name="workAddress" type="text" className="validate" data-testid="workAddress" required={true} />
+                                <label htmlFor="workAddress">Work Address</label>
+                                <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
+                            </div>
+                        </div>
+
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <input id="email" type="email" name="email" className="validate" data-testid="email" required={true} />
+                                <label htmlFor="email">Email</label>
+                                <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
+                            </div>
+                        </div>
                         
-                        <div className="input-field col s6">
-                            <input  id="homeAddress" name="homeAddress" type="text" className="validate" data-testid="homeAddress" required={true}/>
-                            <label htmlFor="homeAddress">Home Address</label>
-                            <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
+                        <div className="row">
+                            <div className="input-field col s6">
+                                <input id="password" name="password" type="password" className="validate" data-testid="password" required={true}/>
+                                <label htmlFor="password">Password</label>
+                                <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
+                            </div>
+                            <div className="input-field col s6">
+                                <input id="confirmPassword" name="confirmPassword" type="password" className="validate" required={true} data-testid="confirmPassword" />
+                                <label htmlFor="confirmPassword">Confirm Password</label>
+                                <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
+                            </div>
                         </div>
 
-                        <div className="input-field col s6">
-                            <input id="workAddress" name="workAddress" type="text" className="validate" data-testid="workAddress" required={true} />
-                            <label htmlFor="workAddress">Work Address</label>
-                            <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
+                        <div className="row">
+                            <div className="input-field col s12" >
+                                <button className="waves-effect waves-light btn" data-testid="signupBtn">Submit</button>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="row">
-                        <div className="input-field col s12">
-                            <input id="email" type="email" name="email" className="validate" data-testid="email" required={true} />
-                            <label htmlFor="email">Email</label>
-                            <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
-                        </div>
-                    </div>
-                    
-                    <div className="row">
-                        <div className="input-field col s6">
-                            <input id="password" name="password" type="password" className="validate" data-testid="password" required={true}/>
-                            <label htmlFor="password">Password</label>
-                            <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
-                        </div>
-                        <div className="input-field col s6">
-                            <input id="confirmPassword" name="confirmPassword" type="password" className="validate" required={true} data-testid="confirmPassword" />
-                            <label htmlFor="confirmPassword">Confirm Password</label>
-                            <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
-                        </div>
-                    </div>
-
-                    <div className="row">
-                        <div className="input-field col s12" >
-                            <button className="waves-effect waves-light btn" data-testid="signupBtn">Submit</button>
-                        </div>
-                    </div>
-
-                </form>
+                    </form>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 };
 
 const mapStateToState = (state, ownProps)=>{
@@ -78,4 +94,11 @@ const mapStateToState = (state, ownProps)=>{
     });
 }
 
-export default connect(mapStateToState)(Signup);
+const mapDispatchToState = (dispatch, ownProps)=>{
+    return({
+        addErrors: (errors)=>{dispatch(addErrors(errors))},
+        removeErrors: ()=>{dispatch(removeErrors())}
+    })
+}
+
+export default connect(mapStateToState, mapDispatchToState)(Signup);
