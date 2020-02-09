acdfe168e067434237f00d521558c1ffb3689b0c
Francis Njuguna <coulsorfrancois@gmail.com>
2020-02-09 17:56:58 +0300

    Creates the actionCreator SIGNUP_SUCCESS action type.

---
+++ src/redux/actionCreators/signupSuccess.js
@@ -0,0 +1,9 @@
+import {SIGNUP_SUCCESS} from '../actionTypes';
+
+const signupSuccess = ()=>{
+    return {
+        type: SIGNUP_SUCCESS
+    }
+}
+
+export default signupSuccess;
--- src/redux/actionTypes.js
+++ src/redux/actionTypes.js
@@ -1,6 +1,7 @@
 
 export const LOGIN = 'LOGIN';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS';
 export const LOGOUT = 'LOGOUT';
 export const ADD_ERRORS = 'ADD_ERRORS';
 export const REMOVE_ERRORS = 'REMOVE_ERRORS';
