a7f19e7d83ee9b5289d7252f0fdd28dd0469e8cc
Francis Njuguna <coulsorfrancois@gmail.com>
2020-02-09 21:34:37 +0300

    Setup the async signup action creator

---
+++ src/redux/actionCreators/signup.js
@@ -0,0 +1,23 @@
+import startCall from './startCall';
+import addErrors from './addErrors';
+import endCall from './endCall';
+import axios from 'axios';
+import signupSuccess from './signupSuccess';
+
+const signup = (userDetails)=>{
+    return (dispatch)=>{
+        dispatch(startCall());
+        console.log(userDetails);
+        return axios.post('/users/signup', userDetails).then((response)=>{
+                console.log(response);
+                dispatch(endCall());
+                dispatch(signupSuccess());
+        }).catch((err)=>{
+            let errors = err.response.data
+            dispatch(addErrors(errors));   
+            dispatch(endCall());
+        });
+    }
+}
+
+export default signup;
