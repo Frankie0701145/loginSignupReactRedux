791be8fc9a28ef6376f009537bb75586e096fcdf
Francis Njuguna <coulsorfrancois@gmail.com>
2020-02-09 18:04:21 +0300

    Import the Signup component that is not connected with the redux store for testing.

--- src/test/Signup.test.js
+++ src/test/Signup.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Signup from '../containers/Signup';
+import {Signup} from '../containers/Signup';
 
 test('test the rendering of the Signup component', () => {
     render(<Signup />);
