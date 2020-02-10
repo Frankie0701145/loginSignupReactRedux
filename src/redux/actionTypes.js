1297d84286ccce9d015a64d6200e88bc4c8138b1
Francis Njuguna <coulsorfrancois@gmail.com>
2020-02-10 09:09:16 +0300

    Define the ADD_USER_DETAILS and REMOVE_USER_DETAILS action types. Define the the addUserDetails action creator.

--- src/redux/actionTypes.js
+++ src/redux/actionTypes.js
@@ -6,3 +6,5 @@ export const ADD_ERRORS = 'ADD_ERRORS';
 export const REMOVE_ERRORS = 'REMOVE_ERRORS';
 export const START_CALL  = 'START_CALL';
 export const END_CALL = 'END_CALL';
+export const ADD_USER_DETAILS = 'ADD_USER_DETAILS';
+export const REMOVE_USER_DETAILS = 'REMOVE_USER_DETAILS';
