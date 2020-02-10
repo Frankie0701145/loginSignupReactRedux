8e4b25d278ef574da54245fc84902288c0771c85
Francis Njuguna <coulsorfrancois@gmail.com>
2020-02-10 10:02:15 +0300

    Define the userDetails reducers and connect to the main reducer.

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,12 +2,13 @@
 import authenticate from './authenticate';
 import addRemoveErrors from './addRemoveErrors';
 import startEndCall from './startEndCall';
-
+import userDetails from './setUserDetails';
 
 
 const ujumbeApp = (state={}, action)=>{
     return{
         signedIn: authenticate(state.signedIn, action),
+        userDetails: userDetails(state.user, action),
         errors: addRemoveErrors(state.errors, action),
         isFetching: startEndCall(state.isFetching, action)
     }
---
+++ src/redux/reducers/userDetails.js
@@ -0,0 +1,15 @@
+import {REMOVE_USER_DETAILS, ADD_USER_DETAILS} from './actionTypes';
+
+
+const userDetails = (state = {}, action)=>{
+    switch(action.type){
+        case REMOVE_USER_DETAILS:
+            return {};
+        case ADD_USER_DETAILS:
+            return action.userDetails;
+        default:
+            return state;
+    }
+};
+
+export default userDetails;
